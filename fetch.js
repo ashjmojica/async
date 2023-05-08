a79733f0b49d485039eecd71a21cf4df77d8d958
Ashley Mojica <ashjmojica@gmail.com>
2023-05-08 11:08:28 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,15 +1,14 @@
-console.log("fetching");
-
 async function getPokemon() {
   const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon", []);
   const json = await pokemon.json();
-  console.log(json.results);
   const body = document.querySelector("body");
   for (const poke of json.results) {
     const name = document.createElement("hi");
+    const url = document.createElement("h2");
     name.innerHTML = poke.name;
-    body.append(name);
-    console.log(poke.name);
+    url.innerHTML = poke.name;
+    url.href = poke.url;
+    body.append(name, url);
   }
 }
 getPokemon();
